6c8cd605eedd2c1f368cfd3d0d0f675baeed3a02
ravindra-tls <ravindra.singh@transformative.in>
2025-04-19 13:00:29 +0530

    Update PractitionerRecommendationSection.js

--- components/PractitionerRecommendationSection.js
+++ components/PractitionerRecommendationSection.js
@@ -1,5 +1,3 @@
-// components/PractitionerRecommendationSection.js
-
 import Image from 'next/image';
 
 export default function PractitionerRecommendationSection() {
@@ -16,14 +14,16 @@ export default function PractitionerRecommendationSection() {
 
         {/* Practitioner Cards */}
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 mt-12 justify-items-center">
+
           {/* Practitioner 1 */}
           <div className="flex flex-col items-center">
             <Image
-              src="/images/shanel-miller.png" // Replace with your image
+              src="https://cdn05.zipify.com/aT9Gs1jnmBTtS9pt4L6C0FRJld8=/fit-in/1940x0/3cd02bf12f4c4f22a11ce8cd79f61781/shanel-photo.png"
               alt="Shanel Miller"
               width={160}
               height={200}
               className="rounded-md"
+              unoptimized
             />
             <div className="bg-black text-yellow-400 text-center mt-4 py-2 px-4 w-full font-bold text-sm leading-tight border border-yellow-500">
               SHANEL MILLER<br />
@@ -34,17 +34,19 @@ export default function PractitionerRecommendationSection() {
           {/* Practitioner 2 */}
           <div className="flex flex-col items-center">
             <Image
-              src="/images/jennifer-maklan.png" // Replace with your image
+              src="https://cdn05.zipify.com/-WkZiMy-Bs9UYbr7BqH-DT1PDUE=/fit-in/1940x0/43e60bbf7f614d76b6f1cefea38fcc5f/jennifer-photo.png"
               alt="Jennifer Maklan"
               width={160}
               height={200}
               className="rounded-md"
+              unoptimized
             />
             <div className="bg-black text-yellow-400 text-center mt-4 py-2 px-4 w-full font-bold text-sm leading-tight border border-yellow-500">
               JENNIFER MAKLAN<br />
               <span className="block font-normal text-white">CERTIFIED AYURVEDIC PRACTITIONER</span>
             </div>
           </div>
+
         </div>
       </div>
     </section>
