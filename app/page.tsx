8c64876f34588d60c2217285ee419b70a372ed43
ravindra-tls <ravindra.singh@transformative.in>
2025-04-21 10:45:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Footer from "@/components/Footer"; 
 import ClinicalResultsSection from "@/components/ClinicalResultsSection";
 import EmotionalIntroSection from "@/components/EmotionalIntroSection";
 import WhatIsAyurvedaSection from "@/components/WhatIsAyurvedaSection";
